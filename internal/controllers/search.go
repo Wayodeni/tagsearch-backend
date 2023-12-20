a8159174247b2d7be7ef58121b6b6309219d2b96
lev sviridov <levgeneral@yandex.ru>
2023-12-20 15:09:09 +0300

    fix 500 on querystring spec symbols at the end of search query

--- internal/controllers/search.go
+++ internal/controllers/search.go
@@ -20,11 +20,31 @@ func NewSearchController(service *service.IndexService) *SearchController {
 }
 
 func (controller *SearchController) Search(c *gin.Context) {
+	const SYMBOLS_TO_REMOVE = "+-=&|><!(){}[]^\"~*?:\\/ "
+	queryString := c.Query("query")
+
+	if len(queryString) != 0 {
+		// Removing querystring query syntax special symbols to prevent 500 error
+		for _, symbolToRemove := range SYMBOLS_TO_REMOVE {
+			if queryString[len(queryString)-1] == byte(symbolToRemove) {
+				queryString = queryString[:len(queryString)-1]
+				break
+			}
+		}
+	}
+
 	searchResults, err := controller.service.Find(&service.SearchDocumentRequest{
-		Query: c.Query("query"),
+		Query: queryString,
 		Tags:  c.QueryArray("tags[]"),
 	})
+
 	if err != nil {
+		if err.Error() == "syntax error" {
+			err = fmt.Errorf("syntax error detected, check your query")
+			log.Println(err)
+			c.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
 		err = fmt.Errorf("error during search: %w", err)
 		log.Println(err)
 		c.AbortWithError(http.StatusInternalServerError, err)
